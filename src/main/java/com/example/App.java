59c9525064c149710f1a60781369054e06391174
quiquecove <quiquehuertalopez@gmail.com>
2023-10-29 22:01:43 +0100

    funcionalidad completa de la app (ver, editar, borrar y añadir Productos)

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,11 +18,17 @@ public class App {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args); // Use ConfigurableApplicationContext
         var repository = context.getBean(ProductRepository.class);
     List<Product> products= List.of(
-            new Product(null,"product1",5.99,1),
-            new Product(null,"product1",5.99,1),
-            new Product(null,"product1",5.99,1)
-
-            );
+            new Product(null, "iPhone 15Pro", 999.99, 1),
+            new Product(null, "Samsung Galaxy S23", 799.99, 1),
+            new Product(null, "Sony PlayStation 5", 499.99, 1),
+            new Product(null, "MacBook Air", 1199.99, 1),
+            new Product(null, "Nike Air Max 270", 129.99, 1),
+            new Product(null, "Samsung 4K QLED TV", 1499.99, 1),
+            new Product(null, "Canon EOS 5D Mark IV", 2499.99, 1),
+            new Product(null, "Sony WH-1000XM4 Wireless Headphones", 349.99, 1),
+            new Product(null, "Apple AirPods Pro", 199.99, 1),
+            new Product(null, "Dell XPS 13 Laptop", 1299.99, 1)
+    );
         repository.saveAll(products);
     }
 }
--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -4,10 +4,10 @@ import com.example.emtity.Product;
 import com.example.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/products")
@@ -18,30 +18,110 @@ public class ProductController {
     public ProductController(ProductRepository repository) {
         this.repository = repository;
     }
-/*
-*GET http://localhost:8080/products
-* */
+
+    /*
+     *GET http://localhost:8080/products
+     * */
     @GetMapping
-    public String findAll(Model model){
+    public String findAll(Model model) {
         //List<Product> products=this.repository.findAll();
-    model.addAttribute("products",this.repository.findAll());
+        model.addAttribute("products", this.repository.findAll());
         return "product-list";
     }
+
     /*
      *POST http://localhost:8080/products/new
      * */
-    @GetMapping("/new")
-    public String getForm(Model model){
-        //List<Product> products=this.repository.findAll();
-        model.addAttribute("products",new Product());
+    @GetMapping("/newProduct")
+    public String getForm(Model model) {
+        List<Product> products=this.repository.findAll();
+        model.addAttribute("products", new Product());
         return "product-form";
     }
 
-    @PostMapping
-    public String save(@ModelAttribute("product") Product product){
-    this.repository.save(product);
-     return "redirect:/products";
+    @PostMapping("/newProduct")
+    public String save(@ModelAttribute("product") Product product) {
+        this.repository.save(product);
+        return "redirect:/products";
     }
+
+    /*
+     * GET http://localhost:8080/products/{id}/view
+     */
+    @GetMapping("/{id}/view")
+    public String viewProduct(@PathVariable Long id, Model model) {
+        Optional<Product> productOptional = repository.findById(id);
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            model.addAttribute("product", product);
+            return "product-view";
+        } else {
+            // Handle the case where the product is not found
+            return "product-not-found"; // You should create a view for this case
+        }
+    }
+
+
+    /*
+     * GET http://localhost:8080/products/{id}/edit
+     */
+    @GetMapping("/{id}/edit")
+    public String editProduct(@PathVariable Long id, Model model) {
+        Optional<Product> productOptional = repository.findById(id);
+
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            model.addAttribute("product", product);
+            return "product-ed";
+        } else {
+            // Handle the case where the product is not found
+            return "product-not-found"; // You should create a view for this case
+        }
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateProduct(@PathVariable Long id, @ModelAttribute Product updatedProduct) {
+        // Find the existing product in the database
+        Optional<Product> productOptional = repository.findById(id);
+
+        if (productOptional.isPresent()) {
+            Product existingProduct = productOptional.get();
+
+            // Update the existing product with the new data
+            existingProduct.setTitle(updatedProduct.getTitle());
+            existingProduct.setPrice(updatedProduct.getPrice());
+            existingProduct.setQuantity(updatedProduct.getQuantity());
+
+            // Save the updated product to the database
+            repository.save(existingProduct);
+
+           // return "redirect:/products/{id}/view"; // Redirect to the product view page
+            return "redirect:/products/{id}/view"; // Redirect to the product view page
+
+        } else {
+            // Handle the case where the product is not found
+            return "product-not-found"; // You should create a view for this case
+        }
+    }
+
+    /*
+     * GET http://localhost:8080/products/{id}/delete
+     */
+    @GetMapping("/{id}/delete")
+    public String deleteProduct(@PathVariable Long id) {
+        this.repository.deleteById(id);
+        return "redirect:/products";
+    }
+
+    /*
+     * GET http://localhost:8080/products/deleteAll
+     */
+    @GetMapping("/deleteAll")
+    public String deleteAllProducts() {
+        this.repository.deleteAll();
+        return "redirect:/products";
+    }
+
     /*
      *GET http://localhost:8080/products/{id}/view
      * */
